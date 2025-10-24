aaa82d338cd1d3be7e6b1a26dd2aa075293e11b0
TonySchrader <schraderantonio@gmail.com>
2025-10-24 13:54:41 -0600

    Create Ejercicio1(6).c

---
+++ Ejercicio1(6).c
@@ -0,0 +1,23 @@
+/*Schrader Rodtriguez Luis Antonio
+clase 2 ejercicio 6*/
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int x = 0, res = 0;
+
+    printf("Dame un numero: ");
+    scanf("%d", &x);
+
+    if (x <= 0)
+    {
+        res = x + 3;
+    }
+    else
+    {
+        res = pow(x, 2) + (x * 2);
+    }
+
+    printf("El resultado es: %d\n", res);
+}
