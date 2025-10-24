506ee50fe38fc5f4b400b1d6f4c62c7fd42b9fad
TonySchrader <schraderantonio@gmail.com>
2025-10-24 13:56:38 -0600

    Add loop to print even numbers from 0 to 10

---
+++ Ejercicio3(8).c
@@ -0,0 +1,12 @@
+/*Schrader Rodtriguez Luis Antonio
+clase 3 ejercicio 8*/
+
+#include <stdio.h>
+
+void main()
+{
+    for (int i = 0; i <= 10; i = i + 2)
+    {
+        printf("Num: %d\n", i);
+    }
+}
