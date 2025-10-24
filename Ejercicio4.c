202a0ddbfaa4c246001b7435a5523e4d29006d31
TonySchrader <schraderantonio@gmail.com>
2025-10-24 14:00:24 -0600

    Change loop from incrementing to decrementing

---
+++ Ejercicio4.c
@@ -0,0 +1,12 @@
+/*Schrader Rodtriguez Luis Antonio
+clase 3 ejercicio 4*/
+
+#include <stdio.h>
+
+void main()
+{
+    for (int i = 100; i >= 1; i--)
+    {
+        printf("Num: %d\n", i);
+    }
+}
