096c79baf5fc2b5f0e4295d2adf8d518abf3ee68
TonySchrader <schraderantonio@gmail.com>
2025-10-24 14:08:19 -0600

    Change loop to count from 1 to 100
    Modified loop to count from 1 to 100 and print multiples of 3.

---
+++ Ejercicio5.c
@@ -0,0 +1,16 @@
+/*Schrader Rodtriguez Luis Antonio
+clase 3 ejercicio 5*/
+
+#include <stdio.h>
+
+void main()
+
+{
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 3 == 0)
+        {
+          printf("Num: %d\n", i);
+        } 
+    }
+}
